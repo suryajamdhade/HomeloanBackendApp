06614778c8de5fe9be22c6eb0660a6b3b8599cc7
kirangajul <kirangajul1998@gmail.com>
2023-02-28 22:20:40 +0530

    Revert "added CRUD in admin module"
    This reverts commit a6a6dc090bbcac39569137f1362e3b9bead490eb.

--- ServiceRegistery/src/main/java/com/homeloan/app/ServiceRegisteryApplication.java
+++ ServiceRegistery/src/main/java/com/homeloan/app/ServiceRegisteryApplication.java
@@ -2,14 +2,21 @@ package com.homeloan.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class ServiceRegisteryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegisteryApplication.class, args);
-	
+		System.out.println("Added by Shubham");
 	}
+
+	public void m1(){
+      
+System.out.println("xyz");
+System.out.println("Added By Amit");
+
+System.out.println("Added by Pratiksha");
+}
+
 }
