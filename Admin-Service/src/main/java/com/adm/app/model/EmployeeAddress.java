6778dd974271b46496e76ad2c0ee81999c9e370c
Ashu-029 <ashishsawale2902@gmail.com>
2023-03-14 15:42:46 +0530

    created new admin service as it was getting currupted

---
+++ Admin-Service/src/main/java/com/adm/app/model/EmployeeAddress.java
@@ -0,0 +1,26 @@
+package com.adm.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeAddress {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int eAddId;
+	private String eState;
+	private String eCity;
+	private String eLandmark;
+	private int pincode;
+	
+}
---
+++ Admin-Service/src/main/java/com/adm/app/repository/AdminRepository.java
@@ -0,0 +1,14 @@
+package com.adm.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;	
+import org.springframework.stereotype.Repository;
+
+import com.adm.app.model.Employee;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Employee, Integer> {
+
+//	void saveEmployeeByempId(int id);
+
+	
+}
